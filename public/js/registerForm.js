39b1b14cac63c11d09bfabb77f5f28782b5a3b8b
chmubarak <chmohamedmubarak@gmail.com>
2022-08-27 18:27:23 +0530

    bug fix in form validation

--- public/js/registerForm.js
+++ public/js/registerForm.js
@@ -1,32 +1,55 @@
 $(document).ready(function () {
-    $("#myForm").validate({
-      errorClass: "my-error-class",
-      rules: {
-        confirmedPassword: {
-          equalTo: "#password"
-        }
+  $("#myForm").validate({
+    errorClass: "my-error-class",
+    rules: {
+      confirmedPassword: {
+        equalTo: "#password"
       },
-      messages: {
-        confirmedPassword: {
-          equalTo: "Password Not matching",
-        }
+      email:{
+        mail:true
+      },
+      name:{
+        name:true
       }
-    })
-
+    },
+    messages: {
+      confirmedPassword: {
+        equalTo: "Password Not matching.",
+      },
+      email:{
+        mail:"Please enter a valid email address."
+      },
+      name:{
+        name:"Numbers and Special character not allowed."
+      }
+    }
   })
 
-  $('#myForm input').on('keyup blur', function () { // fires on every keyup & blur
-    if ($('#myForm').valid()) {                   // checks form for validity
-      $('#registerButton').prop('disabled', false);        // enables button
-    } else {
-      $('#registerButton').prop('disabled', 'disabled');   // disables button
-    }
-  });
+})
+
+jQuery.validator.addMethod("mail", function (value, element) {
+  let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return element.value.match(mailFormat)
+})
+
+jQuery.validator.addMethod("name", function (value, element) {
+  return /^[A-Za-z ]+$/.test(value)
+})
 
 
-  $('#registerButton').click(function () {
-    $('#registerButton').html(`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+
+$('#myForm input').on('keyup blur', function () { // fires on every keyup & blur
+  if ($('#myForm').valid()) {                   // checks form for validity
+    $('#registerButton').prop('disabled', false);        // enables button
+  } else {
+    $('#registerButton').prop('disabled', 'disabled');   // disables button
+  }
+});
+
+
+$('#registerButton').click(function () {
+  $('#registerButton').html(`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
     Loading...`).attr("disabled", 'disabled')
-    $("#myForm").submit()
+  $("#myForm").submit()
 
-  });
+});

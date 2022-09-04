f62359c55ec31ecb57d42fa58ada0f6b7dff5d50
chmubarak <chmohamedmubarak@gmail.com>
2022-09-05 00:52:22 +0530

    add my orders

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const userControl = require("../controllers/userController")
 const authentication = require("../middleware/authentication")
 const wishlistControl = require("../controllers/wishlistController")
 const cartControl = require("../controllers/cartController")
+const Order =require("../models/order")
 const router = express.Router()
 
 router.use(authentication.checkLoggedIn, authentication.checkAccountVerified)
@@ -12,8 +13,15 @@ router.get("/changePassword", userControl.getChangePassword)
 router.get("/wishlist", wishlistControl.wishlist)
 router.get("/cart", cartControl.getCart)
 router.get("/checkout", cartControl.getCheckout)
-router.get("/myorders",(req,res)=>{
-    res.render("master/myOrders")
+router.get("/myorders",async(req,res)=>{
+    try{
+        const userId="63077b79c26f55806c688ede"
+        const orders= await Order.find({userId})
+        // console.log(order)
+        res.render("master/myOrders",{orders:orders})
+    }catch(err){
+        console.log(err)
+    }
 })
 
 router.post("/checkout", cartControl.checkout)

eb70dd30ea8107be8a5fef014b4d90db17ccc5d6
chmubarak <chmohamedmubarak@gmail.com>
2022-09-19 19:20:25 +0530

    fix bugs in filter

--- public/js/sweet-alert.js
+++ public/js/sweet-alert.js
@@ -5,8 +5,8 @@ async function sweetConfirm(orderId) {
             text: "You won't be able to revert this!",
             icon: 'warning',
             showCancelButton: true,
-            confirmButtonColor:'#6F7E8B',
-            cancelButtonColor:'#273952',
+            confirmButtonColor: '#6F7E8B',
+            cancelButtonColor: '#273952',
             confirmButtonText: 'Yes, cancel it!',
             cancelButtonText: 'No.',
             width: '25em'
@@ -26,23 +26,12 @@ async function cancelOrder(orderId) {
             url: `/user/cancelOrder/${orderId}`
         })
         if (response.status === 201) {
+            $(`#order-${orderId}`).load(location.href + ` #order-${orderId}>*`, "");
             Swal.fire(
                 'Cancelled!',
                 'Your order has been cancelled.',
                 'success'
             )
-            document.getElementById(`orderStatusRoad-${orderId}`).innerHTML = `<span class="dot-danger"></span>
-                  <hr class="flex-fill track-line-danger"><span class=""></span>
-                  <hr class="flex-fill track-line-danger"><span class=""></span>
-                  <hr class="flex-fill track-line-danger"><span class=""></span>
-                  <hr class="flex-fill track-line-danger"><span class="d-flex justify-content-center align-items-center big-dot-danger dot">
-                    <i class="fa-sharp fa-solid fa-xmark"></i></span>`
-
-            document.getElementById(`orderStatusUpdate-${orderId}`).innerHTML = `<div class="d-flex flex-column justify-content-center"></div>
-                  <div class="d-flex flex-column justify-content-center align-items-center"></div>
-                  <div class="d-flex flex-column justify-content-center align-items-center"></div>
-                  <div class="d-flex flex-column justify-content-center align-items-center"></div>
-                  <div class="d-flex flex-column align-items-end"><span>Cancelled</span></div>`
         }
     } catch (err) {
         window.location.reload()
